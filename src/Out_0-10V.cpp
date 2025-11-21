ba29190fed14962abf0ba2a0f44aa07e08f5632c
krzychooo <krzysztofklimas@interia.pl>
2025-11-21 17:22:53 +0100

    beafore new branch

--- src/Out_0-10V.cpp
+++ src/Out_0-10V.cpp
@@ -10,10 +10,11 @@ Out_0_10V::~Out_0_10V()
 }
 
 void Out_0_10V::setVoltage(uint16_t miliVolt){
+    if (miliVolt>10000) miliVolt = 10000;
+    
     uint16_t i = map(miliVolt, 0, 10000, this->_limes0V, this->_limes10V);
     Serial.print("map="); Serial.println(i);
     _MCP4725Obj->setValue(i);
-
 }
 
 void Out_0_10V::begin(uint16_t limes0V, uint16_t limes10V){
